98395c5e86b86a675e5ebb76a7e98e89a90d9d8b
OxidisedGearz <108626116+OxidisedGearz@users.noreply.github.com>
2022-07-04 23:15:36 -0500

    Add Datafile Generator Visual Studio Project
    Adds the solution used to generate the datafiles

---
+++ Datafile Generator/Game/TimelessJewel.cs
@@ -0,0 +1,40 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TimelessEmulator.Game.TimelessJewel
+// Assembly: TimelessEmulator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: F88AF474-1AC6-4934-AFAD-2E743166E28C
+// Assembly location: F:\Downloads\TimelessEmulator\TimelessEmulator.dll
+
+using System;
+using DatafileGenerator.Data.Models;
+
+namespace DatafileGenerator.Game
+{
+  public class TimelessJewel
+  {
+    public AlternateTreeVersion AlternateTreeVersion { get; private set; }
+
+    public TimelessJewelConquerer TimelessJewelConquerer { get; private set; }
+
+    public uint Seed { get; private set; }
+
+    public TimelessJewel(
+      AlternateTreeVersion alternateTreeVersion,
+      TimelessJewelConquerer timelessJewelConquerer,
+      uint seed)
+    {
+      ArgumentNullException.ThrowIfNull((object) alternateTreeVersion, nameof (alternateTreeVersion));
+      ArgumentNullException.ThrowIfNull((object) timelessJewelConquerer, nameof (timelessJewelConquerer));
+      this.AlternateTreeVersion = alternateTreeVersion;
+      this.TimelessJewelConquerer = timelessJewelConquerer;
+      this.Seed = seed;
+      this.TransformSeed();
+    }
+
+    private void TransformSeed()
+    {
+      if (this.AlternateTreeVersion.Index != 5U)
+        return;
+      this.Seed /= 20U;
+    }
+  }
+}
---
+++ Datafile Generator/Game/TimelessJewelConquerer.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TimelessEmulator.Game.TimelessJewelConquerer
+// Assembly: TimelessEmulator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: F88AF474-1AC6-4934-AFAD-2E743166E28C
+// Assembly location: F:\Downloads\TimelessEmulator\TimelessEmulator.dll
+
+namespace DatafileGenerator.Game
+{
+  public class TimelessJewelConquerer
+  {
+    public uint Index { get; private set; }
+
+    public uint Version { get; private set; }
+
+    public TimelessJewelConquerer(uint index, uint version)
+    {
+      this.Index = index;
+      this.Version = version;
+    }
+  }
+}
