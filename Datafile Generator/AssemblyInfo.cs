b1f3896f3d6b390cec5d195915603e1f4d316c6b
OxidisedGearz <108626116+OxidisedGearz@users.noreply.github.com>
2022-07-05 10:10:40 -0500

    Delete Datafile Generator directory

--- Datafile Generator/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿//using System.Reflection;
-
-//[assembly: AssemblyCompany("TimelessEmulator")]
-//[assembly: AssemblyConfiguration("Release")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
-//[assembly: AssemblyInformationalVersion("1.0.0")]
-//[assembly: AssemblyProduct("TimelessEmulator")]
-//[assembly: AssemblyTitle("TimelessEmulator")]
-//[assembly: AssemblyVersion("1.0.0.0")]
